5d6569331bab259305cb222cb978eba9877003da
Pathlib <gautroncyber@proton.me>
2026-02-07 15:39:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def question():
         return a
     except KeyboardInterrupt:
         pass
-   
+
 def suppresion():
     print("supretion")
     print(liste)
@@ -28,19 +28,25 @@ def reponse():
 def repbool():
     h=int(input("numeros de la question : "))
     print(liste[h])
-    
-    
-    
+    valeur=input("valeur booeene : ")
+    if valeur == "True" or "False":
+        g=liste[h]+valeur
+        print (g)
+    else:
+         print("erreur")
+
 
 
 
 while True:
     question()
     print(liste)
-    z=input("fin/supr : ")
+    z=input("fin/supr/bool : ")
     if z == "fin":
         reponse()
         print(liste)
         print(liste[0])
     elif z == "supr":
         suppresion()
+    elif z == "bool":
+        repbool()
